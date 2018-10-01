71f2ba817649bc14ddd32161ced64fecd2590bba
roopam527 <roopamg777@gmail.com>
2018-10-02 01:37:58 +0530

    confag data 2

--- dbconfig.js
+++ dbconfig.js
@@ -3,7 +3,7 @@
 const knex = require('knex')({
     client: 'mysql',
     connection: {
-      host : 'http://iacc706.000webhostapp.com',
+      host : 'http://iacc706.000webhostapp.com/',
       user : 'id7265702_iot_team',
       password : 'iot@123',
       database : 'id7265702_iot_database'
